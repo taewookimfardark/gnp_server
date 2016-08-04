d7ef96ebb6389190950bac06442bb6ba9ebe5614
taewookimfardark <2012142087@yonsei.ac.kr>
2016-08-04 13:31:05 +0900

    경기기록 post 요청

--- application/api/match.py
+++ application/api/match.py
@@ -30,3 +30,23 @@ def get_matches():
     return jsonify(
         data = query_list
     )
+
+@api.route('/matches', methods=['POST'])
+def post_matches():
+    request_params = request.get_json()
+    tag = request_params.get('tag')
+    against = request_params.get('against')
+    where = request_params.get('where')
+    when = request_params.get('when')
+    matchinfo = request_params.get('matchinfo')
+    score_gnp = request_params.get('score_gnp')
+    score_enemy = request_params.get('score_enemy')
+    finish = request_params.get('finish')
+    win = request_params.get('win')
+
+    match = Match(tag=tag,against=against,where=where,when=when,matchinfo=matchinfo,score_gnp=score_gnp,score_enemy=score_enemy,finish=finish,win=win)
+    db.session.add(match)
+    db.session.commit()
+    return jsonify(
+        data=model_to_dict(match),
+    )
