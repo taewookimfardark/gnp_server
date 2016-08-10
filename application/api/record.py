bdba8644d30860e898c7a6e1df0d2953134b0125
taewookimfardark <2012142087@yonsei.ac.kr>
2016-08-10 15:09:24 +0900

    선수기록 페이지 요청

--- application/api/record.py
+++ application/api/record.py
@@ -20,6 +20,30 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+@api.route('/recordplayers', methods=['GET'])
+def get_record_players():
+    player_records = PlayerRecord.query.all()
+    query_list = []
+    for record in player_records:
+        temp = model_to_dict(record)
+        print temp
+        query_list.append(temp)
+    return jsonify(
+        data = query_list
+    )
+
+@api.route('/recordmatches', methods=['GET'])
+def get_record_matches():
+    match_records = MatchRecord.query.all()
+    query_list = []
+    for record in match_records:
+        temp = model_to_dict(record)
+        query_list.append(temp)
+    return jsonify(
+        data = query_list
+    )
+
+
 @api.route('/recordusers',methods=['POST'])
 def post_record_users():
     request_params = request.get_json()
@@ -37,6 +61,7 @@ def post_record_users():
 
 @api.route('/recordmatches/<int:matchid>', methods=['PUT'])
 def put_match_user_record(matchid):
+    print request.get_json()
     match = Match.query.get(matchid)
     matchparam = request.get_json()[0]
     match.finish = matchparam.get('finish')
@@ -44,24 +69,41 @@ def put_match_user_record(matchid):
     match.matchinfo = matchparam.get('matchinfo')
     match.score_gnp = matchparam.get('scoregnp')
     match.score_enemy = matchparam.get('scoreenemy')
+    db.session.add(match)
     db.session.commit()
 
     userparam = request.get_json()[1]
     for param in userparam:
-        print(param.get('point'))
-        print(type(param.get('point')))
-        temp_user = db.session.query(PlayerRecord).filter(PlayerRecord.userid==param.get('userid')).one()
-        print(temp_user.points)
-        print(type(temp_user.points))
-        print(temp_user.points + param.get('point'))
-        print(type(temp_user.points + param.get('point')))
-        temp_user.games = temp_user.games+1
-        temp_user.points = temp_user.points + param.get('point')
-        temp_user.assists = temp_user.assists + param.get('assist')
-        temp_user.rebounds = temp_user.rebounds + param.get('rebound')
+        print param
+        if(param.get('ischecked')==True):
+            # print param
+            # print param.get('userid')
+            # print type(param.get('userid'))
+            print "loop"
+            temp_user = db.session.query(PlayerRecord).filter(PlayerRecord.userid==int(param.get('userid'))).first()
+            print temp_user
+            temp_user.games = int(temp_user.games)+1
+            temp_user.points = int(temp_user.points) + param.get('point')
+            temp_user.assists = int(temp_user.assists) + param.get('assist')
+            temp_user.rebounds = int(temp_user.rebounds) + param.get('rebound')
+            db.session.add(temp_user)
     db.session.commit()
 
     return "success"
+
+@api.route('/playerrecord/page', methods=['GET'])
+def player_record_page():
+    playerrecords = PlayerRecord.query.all()
+    data = []
+    for playerrecord in playerrecords:
+        temp = []
+        temp_user_data = User.query.get(playerrecord.userid)
+        temp.append(model_to_dict(temp_user_data))
+        temp.append(model_to_dict(playerrecord))
+        data.append(temp)
+    return jsonify(
+        data=data
+    )
 
 
 
